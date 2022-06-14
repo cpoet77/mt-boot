b7b4337dab54baec78e5f406006c05f7162e1344
llzero54 <llzero54@foxmail.com>
2022-06-14 18:12:05 +0800

    test: 测试多租户数据源

--- mt-boot/mt-web-single/src/test/java/cn/cpoet/mt/single/DBMigrationTest.java
+++ mt-boot/mt-web-single/src/test/java/cn/cpoet/mt/single/DBMigrationTest.java
@@ -1,5 +1,11 @@
 package cn.cpoet.mt.single;
 
+import cn.cpoet.mt.api.constant.SystemConst;
+import cn.cpoet.mt.db.tenant.TenantDb;
+import cn.cpoet.mt.db.tenant.TenantDbContext;
+import cn.cpoet.mt.model.domain.StaffRole;
+import cn.cpoet.mt.model.domain.query.QStaffRole;
+import io.ebean.Database;
 import io.ebean.annotation.Platform;
 import io.ebean.dbmigration.DbMigration;
 import org.junit.jupiter.api.Test;
@@ -29,5 +35,36 @@ public class DBMigrationTest {
         dbMigration.addPlatform(Platform.ORACLE);
         dbMigration.setVersion("1.0.0");
         dbMigration.generateMigration();
+    }
+
+    @Test
+    public void tenantTest() {
+        TenantDbContext instance = TenantDbContext.getInstance();
+//        Database master = instance.getMaster(SystemConst.SYS_TENANT_ID);
+//        Database salve = instance.getSalve(SystemConst.SYS_TENANT_ID);
+
+        StaffRole staffRole = new StaffRole();
+        staffRole.setStaffId(-1L);
+        staffRole.setRoleId(-1L);
+        staffRole.setDeleted(Boolean.FALSE);
+//        master.save(staffRole);
+        staffRole.save();
+
+        System.out.println(staffRole);
+
+//        StaffRole one = new QStaffRole(salve)
+//            .id.eq(staffRole.getId())
+//            .findOne();
+
+        System.out.println(TenantDb
+            .agent(QStaffRole::new)
+            .id.eq(staffRole.getId())
+            .findOne());
+
+        System.out.println(TenantDb
+            .agent(QStaffRole.class)
+            .id.eq(staffRole.getId())
+            .findOne());
+
     }
 }
